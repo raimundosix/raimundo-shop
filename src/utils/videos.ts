08b698b5167343f63a3ee55f224fcc0c77ff3c92
kauan777 <kauan7622@gmail.com>
2022-11-06 20:22:52 -0300

    adicionando novos videos

--- src/utils/videos.ts
+++ src/utils/videos.ts
@@ -7,21 +7,31 @@ export type VideoProps = {
 export const videos: VideoProps[] = [
   {
     id: 1,
+    title: "PAINEL HYDRA",
+    srcVideo: "/videos/HYDRA-SITE.mp4",
+  },
+  {
+    id: 2,
+    title: "PAINEL BUSCA DADOS",
+    srcVideo: "/videos/BUSCA_DADOS.mp4",
+  },
+  {
+    id: 3,
     title: "PAINEL CYBER BUSCAS",
     srcVideo: "/videos/CYBER-SITE.mp4",
   },
   {
-    id: 2,
+    id: 4,
     title: "PAINEL KING BUSCAS",
     srcVideo: "/videos/KING-SITE.mp4",
   },
   {
-    id: 3,
+    id: 5,
     title: "PAINEL CH BUSCA",
     srcVideo: "/videos/CH_BUSCA-SITE.mp4",
   },
   {
-    id: 4,
+    id: 6,
     title: "PAINEL DATAPRO",
     srcVideo: "/videos/DATAPRO_SITE.mp4",
   },
